e81a0d677e1bc8ac711d03387e25347d8b68bca2
unknown <aadityakothawade72@gmail.com>
2025-06-27 14:08:05 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,23 +4,36 @@ import dotenv from 'dotenv';
 import notesRoutes from './routes/notesRoutes.js';
 import {connectDB} from './config/db.js';
 import rateLimiter from './middleware/ratelimiter.js';
+import path from 'path';
 
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const __dirname = path.resolve();
 
 app.use(express.json()); // Middleware to parse JSON request bodies
-app.use(cors({
+if(process.env.NODE_ENV !== 'production') {
+    app.use(cors({
     origin :"http://localhost:5173"
+    }));
+}
 
-}));
 app.use(rateLimiter)
 
 
 app.use("/api/notes", notesRoutes);
- 
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get(/^\/(?!api).*/, (req, res) => {
+         res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+
+}
+
+
 connectDB().then(()=>{
     app.listen(PORT,()=>{
         console.log('Server is running on port ', PORT);
@@ -29,4 +42,3 @@ connectDB().then(()=>{
  
 
 
-//mongodb+srv://aadityakothawade72:6qBxSH4OPcD6iJEx@cluster0.lqqnsym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
